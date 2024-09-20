e0702197a75de13e646af35d9febea408efe8da2
zzz0906 <zzz879978@outlook.com>
2024-09-19 22:31:53 -0700

    to merge

--- scripts/throughput.sh
+++ scripts/throughput.sh
@@ -7,7 +7,7 @@
 # --use_flash_attention \
 # -semi-compiled
 
-model_name="gpt_dense:llama-3-8b"
+model_name="gpt_ladder:llama-3-8b"
 folder=./logs/tmp/
 mkdir -p ${folder}
 for bssize in 4
@@ -15,12 +15,13 @@ do
     for tpsize in 4
     do
         echo "Running with bs=${bssize} tp=${tpsize}"
-        CUDA_LAUNCH_BLOCKING=1 TORCH_USE_CUDA_DSA=1 TORCHDYNAMO_VERBOSE=1 ENABLE_INTRA_NODE_COMM=1 torchrun --standalone --nproc_per_node=${tpsize} benchmark.py \
+        ENABLE_INTRA_NODE_COMM=1 torchrun --standalone --nproc_per_node=${tpsize} benchmark.py \
                                         --model_name ${model_name} \
                                         --num_samples 10 \
                                         --batch_size ${bssize} \
-                                        --prompt_length 1024 \
-                                        --max_new_tokens 512 \
+                                        --prompt_length 10 \
+                                        --max_new_tokens 10 \
+                                        --cuda_graph \
                                         --use_flash_attention \
                                         --device cuda > ${folder}/model_name_bssize${bssize}_tpsize${tpsize}_91.log 2>&1
         echo "Finished running with bs=${bssize} tp=${tpsize}" 
