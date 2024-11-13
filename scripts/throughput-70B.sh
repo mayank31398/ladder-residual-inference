6557f003a57bbdb1ee38253cb9580a3d85fd494b
zzz0906 <zzz879978@outlook.com>
2024-11-12 22:37:40 -0800

    to merge the code

--- scripts/throughput-70B.sh
+++ scripts/throughput-70B.sh
@@ -1,25 +1,27 @@
-NVLINK=0
 mode=cuda_graph_use_flash_attention
-export NCCL_P2P_DISABLE=${NVLINK}
-for model_name in "gpt_dense:llama-3-70b"
+for P2P_DISABLE in 0 1
 do
-    folder=./logs/09_20_float16_disable${NVLINK}/${mode}/${model_name}
-    mkdir -p ${folder}
-    for bssize in 8
+    export NCCL_P2P_DISABLE=${P2P_DISABLE}
+    for model_name in "gpt_ladder:llama-3-70b" "gpt_dense:llama-3-70b"
     do
-        for tpsize in 4
+        folder=./logs/10_02_float16_disable${P2P_DISABLE}/${mode}/${model_name}
+        mkdir -p ${folder}
+        for bssize in 16
         do
-            echo "Running with bs=${bssize} tp=${tpsize}"
-            NCCL_P2P_DISABLE=${NVLINK} torchrun --standalone --nproc_per_node=${tpsize} --master_port=15328 benchmark.py \
-                                            --model_name ${model_name} \
-                                            --num_samples 10 \
-                                            --batch_size ${bssize} \
-                                            --prompt_length 1024 \
-                                            --max_new_tokens 256 \
-                                            --cuda_graph \
-                                            --use_flash_attention \
-                                            --device cuda 2>&1 | tee ${folder}/bs_${bssize}_tp_${tpsize}.log
-            echo "Finished running with bs=${bssize} tp=${tpsize}" 
+            for tpsize in 4 8
+            do
+                echo "Running with bs=${bssize} tp=${tpsize}"
+                NCCL_NVLS_ENABLE=1 NCCL_P2P_DISABLE=${P2P_DISABLE} torchrun --standalone --nproc_per_node=${tpsize} --master_port=15328 benchmark.py \
+                                                --model_name ${model_name} \
+                                                --num_samples 10 \
+                                                --batch_size ${bssize} \
+                                                --prompt_length 1024 \
+                                                --max_new_tokens 256 \
+                                                --cuda_graph \
+                                                --use_flash_attention \
+                                                --device cuda 2>&1 | tee ${folder}/bs_${bssize}_tp_${tpsize}.log
+                echo "Finished running with bs=${bssize} tp=${tpsize}" 
+            done
         done
     done
 done
