f2c434fcb32c10f8dc355081c57a2205e3c35840
zzz0906 <zzz879978@outlook.com>
2024-11-14 16:18:20 -0800

    log for rebuttal

--- gpt_fast/gpt_ensemble_TP.py
+++ gpt_fast/gpt_ensemble_TP.py
@@ -87,7 +87,6 @@ transformer_configs = {
     "llama-3-8b-4x": dict(block_size=8192, n_layer=32, n_head=32, n_local_heads=8, dim=4096, intermediate_size=14336, vocab_size=128256, rope_base=500000, reduce_pattern=[{"attention": False, "mlp": False}, {"attention": False, "mlp": True}] * 16),
     "llama-3-8b-upper-bound": dict(block_size=8192, n_layer=32, n_head=32, n_local_heads=8, dim=4096, intermediate_size=14336, vocab_size=128256, rope_base=500000, reduce_pattern=[{"attention": False, "mlp": False} for _ in range(32)], force_disable_last_all_reduce=True),
     "llama-3-8b-infinite": dict(block_size=8192, n_layer=32, n_head=32, n_local_heads=8, dim=4096, intermediate_size=14336, vocab_size=128256, rope_base=500000, reduce_pattern=[{"attention": False, "mlp": False} for _ in range(32)]),
-    
     "llama-3-70b-4x": dict(block_size=8192, n_layer=80, n_head=64, n_local_heads=8, dim=8192, intermediate_size=28672, vocab_size=128256, rope_base=500000, reduce_pattern=[{"attention": False, "mlp": False}, {"attention": False, "mlp": True}] * 40),
     "llama-3-70b": dict(block_size=8192, n_layer=80, n_head=64, n_local_heads=8, dim=8192, intermediate_size=28672, vocab_size=128256, rope_base=500000),
     "llama-3-70b-upper-bound": dict(block_size=8192, n_layer=80, n_head=64, n_local_heads=8, dim=8192, intermediate_size=28672, vocab_size=128256, rope_base=500000, reduce_pattern=[{"attention": False, "mlp": False} for _ in range(80)], force_disable_last_all_reduce=True),
