ce48edbc48ed9f3882cd3f4fcbd6fb77c8bc4ba9
Mayank Mishra <mayank31398@gmail.com>
2025-01-27 15:04:28 +0530

    70b
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tools/plot-70b.py
+++ tools/plot-70b.py
@@ -14,6 +14,7 @@ def plot(
     ladder: list[float],
     upper_bound: list[float],
     parallel: list[float],
+    extra_title: str,
 ) -> None:
     plt.figure()
 
@@ -25,7 +26,7 @@ def plot(
     plt.xticks(tp_world_size)
     plt.xlim(1.5, 8.5)
 
-    plt.title(title.format(batch_size=batch_size))
+    plt.title(title.format(batch_size=batch_size) + f", {extra_title}")
     plt.ylabel(y_label)
     plt.xlabel(x_label)
     plt.grid(True, linestyle=":", color="gray", linewidth=0.5)
@@ -33,6 +34,7 @@ def plot(
     plt.tight_layout()
     plt.savefig(f"70b-{batch_size}.png", dpi=300)
 
+extra_title = "NVLink enabled"
 
 plot(
     batch_size=1,
@@ -40,6 +42,7 @@ plot(
     ladder=[36.69, 67.51, 101.22],
     upper_bound=[38.24, 69.04, 110.59],
     parallel=[36.67, 65.4, 94.22],
+    extra_title=extra_title,
 )
 
 plot(
@@ -48,6 +51,7 @@ plot(
     ladder=[126.09, 204.3, 331.45],
     upper_bound=[130.77, 213.73, 355.8],
     parallel=[123.66, 201.62, 307.34],
+    extra_title=extra_title,
 )
 
 plot(
@@ -56,6 +60,7 @@ plot(
     ladder=[float("nan"), 373.33, 596.73],
     upper_bound=[float("nan"), 389.03, 646.18],
     parallel=[float("nan"), 363.29, 555.88],
+    extra_title=extra_title,
 )
 
 plot(
@@ -64,4 +69,5 @@ plot(
     ladder=[float("nan"), 1358.65, 2242.1],
     upper_bound=[float("nan"), 1433.53, 2474.49],
     parallel=[float("nan"), 1311.54, 2259.38],
+    extra_title=extra_title,
 )
